8d5112d8a795e51d07a8acc341531a93af268451
Jesse Gall <jesse-gall@hotmail.com>
2023-12-30 18:46:09 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, computed, OnInit, Signal } from '@angular/core';
 import { ActivatedRoute, Route, Router, RouterOutlet } from '@angular/router';
 import { CurrentRoute } from "../../projects/pandora/src/lib/utils/router/current-route";
 import Prism from "prismjs";
-import { NotificationService } from "../../projects/pandora/src/lib/services";
+import { CacheService, NotificationService } from "../../projects/pandora/src/lib/services";
 
 @Component({
     selector: 'app-root',
@@ -22,9 +22,17 @@ export class AppComponent implements OnInit {
 
     constructor(
         private readonly router: Router,
+        private readonly cacheService: CacheService,
         private readonly notificationService: NotificationService,
     ) {
         this.currentRoute = new CurrentRoute();
+
+        this.cacheService.set('test', 'test');
+        if (this.cacheService.get('test') !== 'test') {
+            console.error('CacheService is not working');
+        } else {
+            console.log('CacheService is working');
+        }
     }
 
     ngOnInit() {
